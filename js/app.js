4f21381a884b59e883117c9922cc0f115e71dea4
unknown <quentinadrieu@yahoo.com>
2021-11-03 17:03:40 +0100

    theme_sombre

--- js/app.js
+++ js/app.js
@@ -2,16 +2,21 @@ document.addEventListener("DOMContentLoaded", () =>{
   /*===== MENU SHOW Y HIDDEN =====*/
 const navMenu = document.getElementById('nav-menu'),
 toggleMenu = document.getElementById('nav-toggle'),
-closeMenu = document.getElementById('nav-close')
+closeMenu = document.getElementById('nav-close'),
+main = document.querySelector("main"),
+header = document.querySelector("header")
 
 // SHOW
 toggleMenu.addEventListener('click', ()=>{
 navMenu.classList.toggle('show')
+main.style.display = "none"
+
 })
 
 // HIDDEN
 closeMenu.addEventListener('click', ()=>{
 navMenu.classList.remove('show')
+main.style.display = "block"
 })
   
 })
